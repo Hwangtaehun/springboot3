4b7cc759aa1ced9637d33ad45d28dc0da09835de
Hwangtaehun <r283@naver.com>
2025-09-04 19:28:09 +0900

    업로드

---
+++ springboot-developer/src/main/java/me/hwangtaehun/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,7 @@
+package me.hwangtaehun.springbootdeveloper;
+
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args){
+        SprinApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
