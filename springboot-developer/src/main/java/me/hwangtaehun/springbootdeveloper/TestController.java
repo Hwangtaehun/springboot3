8b55a99b45009453d2e0781a746ae534241bc07a
Hwangtaehun <r283@naver.com>
2025-09-08 10:05:28 +0900

    임시 저장

--- springboot-developer/src/main/java/me/hwangtaehun/springbootdeveloper/TestController.java
+++ springboot-developer/src/main/java/me/hwangtaehun/springbootdeveloper/TestController.java
@@ -1,10 +1,15 @@
 package me.hwangtaehun.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Autowired
+    TestService testService;
+
     @GetMapping("/test")
     public String test(){
         return "Hello, world!";
