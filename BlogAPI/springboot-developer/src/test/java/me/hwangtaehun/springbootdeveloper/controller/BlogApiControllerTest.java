ddfa1f29718d8b3c7323c90f60292638633a7666
Hwangtaehun <r283@naver.com>
2025-09-10 21:25:08 +0900

    폴더명 변경

--- BlogAPI/springboot-developer/src/test/java/me/hwangtaehun/springbootdeveloper/controller/BlogApiControllerTest.java
+++ BlogAPI/springboot-developer/src/test/java/me/hwangtaehun/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -12,17 +12,19 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -34,13 +36,13 @@ class BlogApiControllerTest {
     protected ObjectMapper objectMapper;
 
     @Autowired
-    protected WebApplicationContext context;
+    private WebApplicationContext context;
 
     @Autowired
     BlogRepository blogRepository;
 
     @BeforeEach
-    public void mockMvcSetup(){
+    public void mockMvcSetUp() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .build();
         blogRepository.deleteAll();
@@ -49,22 +51,22 @@ class BlogApiControllerTest {
     @DisplayName("addArticle: 블로그 글 추가에 성공한다.")
     @Test
     public void addArticle() throws Exception {
-        //given
+        // given
         final String url = "/api/articles";
         final String title = "title";
         final String content = "content";
         final AddArticleRequest userRequest = new AddArticleRequest(title, content);
 
-        //객체 JSON으로 직렬화
+        // 객체 JSON으로 직렬화
         final String requestBody = objectMapper.writeValueAsString(userRequest);
 
-        //when
-        //설정한 내용을 바탕으로 요청 전송
+        // when
+        // 설정한 내용을 바탕으로 요청 전송
         ResultActions result = mockMvc.perform(post(url)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(requestBody));
 
-        //then
+        // then
         result.andExpect(status().isCreated());
 
         List<Article> articles = blogRepository.findAll();
