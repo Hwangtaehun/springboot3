f9fb8a01683d5b43d6d036fb458e72221f03fbf4
Hwangtaehun <r283@naver.com>
2025-09-10 21:10:47 +0900

    업로드

--- 블로그API만들기/springboot-developer/src/main/java/me/hwangtaehun/springbootdeveloper/controller/BlogApiController.java
+++ 블로그API만들기/springboot-developer/src/main/java/me/hwangtaehun/springbootdeveloper/controller/BlogApiController.java
@@ -14,9 +14,10 @@ public class BlogApiController {
     private final BlogService blogService;
 
     @PostMapping("/api/articles")
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
-        Article saveArticle = blogService.save(request);
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(saveArticle);
+                .body(savedArticle);
     }
 }
---
+++ 블로그API만들기/springboot-developer/src/test/java/me/hwangtaehun/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -0,0 +1,62 @@
+package me.hwangtaehun.springbootdeveloper.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.hwangtaehun.springbootdeveloper.dto.AddArticleRequest;
+import me.hwangtaehun.springbootdeveloper.repository.BlogRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class BlogApiControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected WebApplicationContext context;
+
+    @Autowired
+    BlogRepository blogRepository;
+
+    @BeforeEach
+    public void mockMvcSetup(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        blogRepository.deleteAll();
+    }
+
+    @DisplayName("addArticle: 블로그 글 추가에 성공한다.")
+    @Test
+    public void addArticle() throws Exception {
+        //given
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+        final AddArticleRequest userRequest = new AddArticleRequest(title, content);
+
+        //객체 JSON으로 직렬화
+        final String requestBody = objectMapper.writeValueAsString(userRequest);
+
+        //when
+        //설정한 내용을 바탕으로 요청 전송
+        ResultActions result = mockMvc.perform(post(url))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody);
+    }
+}
